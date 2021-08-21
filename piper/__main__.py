903a370955957bd5490552b7a71d91e395e82711
Cory Sabol <corygrandhamsabol@gmail.com>
2021-08-21 05:50:14 -0400

    remove pycache

--- piper/__main__.py
+++ piper/__main__.py
@@ -1,36 +1,56 @@
 import time
 import os
 import argparse
+import threading
+from pathlib import Path
 
 from pypyr import pipelinerunner
+from pypyr import context
+from pypyr.pypeloaders import fileloader
 
 from rich.console import Console
 from rich.table import Table
 from rich.progress import track
+from rich.progress import Progress
 from rich import inspect
 
 console = Console()
 
-#out = pipelinerunner.main_with_context(pipeline_name='pipelines/test-pipe',
-#        dict_in={'arbkey':'pipe', 'anotherkey':'song'})
-
-context = {}
-pipelinerunner.prepare_and_run(
+def pipeline_thread_function(shared_context):
+    pipelinerunner.prepare_and_run(
         pipeline_name='pipelines/test-pipe',
         working_dir=os.getcwd(),
-        context=context,
+        context=shared_context,
         parse_input=False
-        )
+        )   
 
-inspect(out)
-#while out['finished'] == False:
-#    console.print(out['progress'])
-    #track(range(out['progress']))
-#inspect(pipelinerunner, methods=True)
-#inspect(out, methods=True)
+# Shared context object used by the main thread here (for reads)
+# and by the pipeline itself in a separate thread (for reads and writes)
+# no locking here because idgaf.
+shared_context = context.Context({
+    'finished': False,
+    'progress': 0, 
+    'arbkey':'pipe', 
+    'anotherkey':'song'
+}) 
+pipeline_thread = threading.Thread(target=pipeline_thread_function, args=(shared_context,))
+pipeline_thread.start()
 
-#for step in track(range(100)):
-#   time.sleep(0.3)
+# display progress
+pipeline_definition = fileloader.get_pipeline_definition('pipelines/test-pipe', Path(os.getcwd()))
+total_steps = len(pipeline_definition['steps'])
+completed_tasks = shared_context['progress']
+with Progress() as progress:
+    t1 = progress.add_task("[cyan]Running...", total=total_steps)
+    while not progress.finished: #and completed_tasks < len(pipeline_definition['steps']):
+        if shared_context['progress'] > completed_tasks:
+            progress.update(t1, advance=1)
+        # update count of completed tasks
+        completed_tasks = shared_context['progress']
+
+
+print('done')
+#pipeline_thread.join()
 
 #table = Table(show_header=True, header_style="bold magenta")
 #table.add_column("Date", style="dim", width=12)
