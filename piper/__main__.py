5cfee46425507519373671109053f2ecafdffdfe
Cory Sabol <corygrandhamsabol@gmail.com>
2021-08-29 04:24:07 -0400

    use pypyr api more directly to get desirable status output.

--- piper/__main__.py
+++ piper/__main__.py
@@ -2,18 +2,27 @@ import time
 import os
 import argparse
 import threading
+import logging
 from pathlib import Path
 
 from pypyr import pipelinerunner
 from pypyr import context
 from pypyr.pypeloaders import fileloader
+from pypyr.stepsrunner import StepsRunner
+from pypyr.dsl import Step
 
 from rich.console import Console
 from rich.table import Table
 from rich.progress import track
 from rich.progress import Progress
 from rich import inspect
+from rich.logging import RichHandler
+from rich import print
 
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level=25, format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
 console = Console()
 
 def pipeline_thread_function(shared_context):
@@ -33,44 +42,29 @@ shared_context = context.Context({
     'arbkey':'pipe', 
     'anotherkey':'song'
 }) 
-pipeline_thread = threading.Thread(target=pipeline_thread_function, args=(shared_context,))
-pipeline_thread.start()
-
-# display progress
+#pipeline_thread = threading.Thread(target=pipeline_thread_function, args=(shared_context,))
+#pipeline_thread.start()
 pipeline_definition = fileloader.get_pipeline_definition('pipelines/test-pipe', Path(os.getcwd()))
-total_steps = len(pipeline_definition['steps'])
-completed_tasks = shared_context['progress']
-with Progress() as progress:
-    t1 = progress.add_task("[cyan]Running...", total=total_steps)
-    while not progress.finished: #and completed_tasks < len(pipeline_definition['steps']):
-        if shared_context['progress'] > completed_tasks:
-            progress.update(t1, advance=1)
-        # update count of completed tasks
-        completed_tasks = shared_context['progress']
+stepsrunner = StepsRunner(pipeline_definition, shared_context) 
+steps = stepsrunner.get_pipeline_steps('steps')
+step_count = 0
 
+with console.status('[bold green]Hacking stuff...', spinner='pong'):
+    for step in steps:
+        if step.get('comment', None):
+            console.log(step.get('comment'))
 
-print('done')
+        step_instance = Step(step, stepsrunner)
+        step_instance.run_step(shared_context)
+        
+        if shared_context.get('cmdOut', None):
+            cmd_out = shared_context.get('cmdOut')
+            shared_context['cmdOut'] = ''
+            console.log(cmd_out['stdout'])
+
+        console.log(f'{step["name"]} compelete')
+        step_count += 1
+
+console.print('')
+console.log(':smiley: done')
 #pipeline_thread.join()
-
-#table = Table(show_header=True, header_style="bold magenta")
-#table.add_column("Date", style="dim", width=12)
-#table.add_column("Title")
-#table.add_column("Production Budget", justify="right")
-#table.add_column("Box Office", justify="right")
-#table.add_row(
-#    "Dev 20, 2019", "Star Wars", "$275,000,000", "375,000,000"
-#)
-#table.add_row(
-#    "May 25, 2018",
-#    "[red]Solo[/red]: A Star Wars Story",
-#    "$275,000,000",
-#    "$393,151,347",
-#)
-#table.add_row(
-#    "Dec 15 2017",
-#    "Star Wars Ep. VIII: The Last Jedi",
-#    "$262,000,000",
-#    "[bold]$1,332,539,889[/bold]",
-#)
-
-#console.print(table)
